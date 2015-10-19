aef418fcf4fbfdb79b950a00a18b699bbecfc064
Jepi Humet <jepihumet@gmail.com>
2015-10-19 02:58:54 +0200

    [FEATURE] - HomeController is the default controller which is called if no controller is found in the URL. - index method is the default method that will be called if no action is found in the URL.

---
+++ src/application/controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * HomeController.php
+ *
+ * @package     JepiFW
+ * @author      Jepi Humet Alsius <jepihumet@gmail.com>
+ * @link        http://jepihumet.com
+ */
+class HomeController
+{
+    public function index(){
+        echo "Hello World";
+    }
+
+    public function myfunc(){
+        echo "my func is running";
+    }
+}
