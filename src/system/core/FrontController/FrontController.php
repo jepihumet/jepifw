10faa91514895a1cf629991a63fbf261e86f2989
Jepi Humet <jepihumet@gmail.com>
2015-10-19 02:51:44 +0200

    [FEATURE] - FrontController already working with the first main features

--- src/system/core/FrontController/FrontController.php
+++ src/system/core/FrontController/FrontController.php
@@ -9,53 +9,93 @@
  */
 class FrontController implements FrontControllerInterface
 {
-    const DEFAULT_CONTROLLER    = "IndexController";
-    const DEFAULT_ACTION        = "index";
+    const DEFAULT_CONTROLLER = "HomeController";
+    const DEFAULT_ACTION = "index";
 
-    protected $controller       = self::DEFAULT_CONTROLLER;
-    protected $action           = self::DEFAULT_ACTION;
-    protected $params           = array();
-    protected $basePath         = "mybasepath/";
+    /**
+     * @var Loader
+     */
+    protected $loader = null;
 
-    public function __construct(array $options = array()){
-        if (empty($options)){
+    /**
+     * @var ConfigAbstract
+     */
+    protected $config = null;
+
+    /**
+     * @var DirectoryManager
+     */
+    protected $directoryManager = null;
+
+    protected $controller = self::DEFAULT_CONTROLLER;
+    protected $action = self::DEFAULT_ACTION;
+    protected $params = array();
+    protected $basePath = null;
+
+    public function __construct(array $options = array())
+    {
+        $this->initializeDirectoryManager();
+        $this->initializeConfig();
+        $this->initializeAutoLoader();
+        if (empty($options)) {
             $this->parseUri();
-        }else{
-            if (isset($options['controller'])){
+        } else {
+            if (isset($options['controller'])) {
                 $this->setController($options['controller']);
             }
-            if (isset($options['action'])){
+            if (isset($options['action'])) {
                 $this->setAction($options['action']);
             }
-            if (isset($options['params'])){
+            if (isset($options['params'])) {
                 $this->setParams($options['params']);
             }
         }
     }
 
-    public function parseUri(){
-        $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),'/');
-        $path = preg_replace('/[^a-zA-Z0-9]//','',$path);
+    public function initializeDirectoryManager()
+    {
+        $this->directoryManager = new DirectoryManager();
+    }
 
-        if (strpos($path, $this->basePath) === 0){
-            $path = substr($path, strlen($this->basePath));
+    public function initializeConfig()
+    {
+        $this->config = new Config();
+        $configFiles = $this->directoryManager->listAllFilesInDirectory(ROOT . DS . 'application' . DS . 'config');
+        foreach ($configFiles as $configFile) {
+            $this->config->loadConfigFile($configFile);
         }
+        $this->basePath = $this->config->get('basePath');
+    }
 
+    public function initializeAutoLoader()
+    {
+        $autoLoader = new AutoLoader($this->directoryManager);
+        $this->loader = new Loader($autoLoader);
+    }
+
+    public function parseUri()
+    {
+        $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), DS);
+        //$path = preg_replace('/[^a-zA-Z0-9]/', '', $path);
+
+        $path = substr($path, strlen($this->basePath));
         @list($controller, $action, $params) = explode('/', $path, 3);
-        if (isset($controller)){
+
+        if (isset($controller) && $controller != "") {
             $this->setController($controller);
         }
-        if (isset($action)){
+        if (isset($action) && $action != "") {
             $this->setAction($action);
         }
-        if (isset($params)){
-            $this->setParams(explode('/',$params));
+        if (isset($params) && $params != "") {
+            $this->setParams(explode(DS, $params));
         }
     }
+
     public function setController($controller)
     {
-        $controller = ucfirst(strtolower($controller)).'Controller';
-        if (!class_Exists($controller)){
+        $controller = ucfirst(strtolower($controller)) . 'Controller';
+        if (!class_exists($controller)) {
             throw new InvalidArgumentException("The action controller '$controller' has not been defined.");
         }
         $this->controller = $controller;
@@ -65,7 +105,7 @@ class FrontController implements FrontControllerInterface
     public function setAction($action)
     {
         $reflector = new ReflectionClass($this->controller);
-        if (!$reflector->hasMethod($action)){
+        if (!$reflector->hasMethod($action)) {
             throw new InvalidArgumentException("The controller action '$action' has been not defined.");
         }
         $this->action = $action;
