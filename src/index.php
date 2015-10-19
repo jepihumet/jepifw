7bb6bce1c89df92cce146f50e63a74b4513532f3
Jepi Humet <jepihumet@gmail.com>
2015-10-19 02:48:36 +0200

    [TASK] - index.php file is adding all required classes to run the FrontController and then calls it

--- src/index.php
+++ src/index.php
@@ -8,10 +8,26 @@
  */
 
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(dirname(__FILE__)));
+define('ROOT', dirname(__FILE__));
 
-require_once ROOT . "/system/library/Loader/DIContainer.php";
-$autoloader = new DIContainer();
+
+require_once ROOT . DS . 'system' . DS . 'config' . DS . 'ConfigAbstract.php';
+require_once ROOT . DS . 'system' . DS . 'config' . DS . 'Config.php';
+
+require_once ROOT . DS . 'system' . DS . 'core' . DS . 'JepiException.php';
+
+require_once ROOT . DS . 'system' . DS . 'library' . DS . 'DirectoryManager' . DS . 'DirectoryManagerInterface.php';
+require_once ROOT . DS . 'system' . DS . 'library' . DS . 'DirectoryManager' . DS . 'DirectoryManager.php';
+
+require_once ROOT . DS . 'system' . DS . 'core' . DS . 'AutoLoader' . DS . 'AutoLoaderException.php';
+require_once ROOT . DS . 'system' . DS . 'core' . DS . 'AutoLoader' . DS . 'AutoLoaderInterface.php';
+require_once ROOT . DS . 'system' . DS . 'core' . DS . 'AutoLoader' . DS . 'AutoLoader.php';
+require_once ROOT . DS . 'system' . DS . 'core' . DS . 'AutoLoader' . DS . 'AutoLoaderPath.php';
+
+require_once ROOT . DS . 'system' . DS . 'core' . DS . 'Loader' . DS . 'Loader.php';
+
+require_once ROOT . DS . 'system' . DS . 'core' . DS . 'FrontController' . DS . 'FrontControllerInterface.php';
+require_once ROOT . DS . 'system' . DS . 'core' . DS . 'FrontController' . DS . 'FrontController.php';
 
 $frontController = new FrontController();
 $frontController->run();
