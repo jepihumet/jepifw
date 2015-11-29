ea085a98a0c20679a1c45ac740371fa1036f71b7
Jepi Humet <jepihumet@gmail.com>
2015-11-30 00:29:29 +0100

    [TASK] - Updating DI trying to allow inheirtance

--- JepiFw/System/DependencyInjection/DI.php
+++ JepiFw/System/DependencyInjection/DI.php
@@ -10,35 +10,60 @@ namespace Jepi\Fw\DependencyInjection;
  * @author      Jepi Humet Alsius <jepihumet@gmail.com>
  * @link        http://jepihumet.com
  */
-class DI {
+class DI
+{
     static private $container = array();
 
-    public static function addClass($className, $instance){
+    public static function addClass($className, $instance)
+    {
         self::$container[$className] = $instance;
     }
 
-    public static function get($className, $parameters = array()){
-        if (in_array($className, array_keys(self::$container))){
-            //class exists
-            return self::$container[$className];
-        }else{
-            //class does not exists
-            $reflectionClass = new \ReflectionClass($className);
-
-            $constructorReflection = $reflectionClass->getConstructor();
-            $reflectionParameters = $constructorReflection->getParameters();
-            $arguments = array();
-            foreach($reflectionParameters as $reflectionParameter){
-                $paramName = $reflectionParameter->getName();
-                $paramClass = $reflectionParameter->getClass()->name;
-
-                $paramInstance = DI::get($paramClass);
-                $arguments[$paramName] = $paramInstance;
-            }
-            $instance = $reflectionClass->newInstanceArgs($arguments);
-            self::$container[$className] = $instance;
-
-            return self::$container[$className];
+    public static function get($className, $parameters = array())
+    {
+        $instance = self::getFromCurrentInstances($className);
+        if ($instance != false) {
+            return $instance;
         }
+        //If no class instance found then setup the original className and start with the instance creation
+        $reflectionClass = new \ReflectionClass($className);
+        if ($reflectionClass->isFinal()) {
+            $constructorReflection = $reflectionClass->getConstructor();
+            jlog("CONSTRUCTOR", $constructorReflection);
+            $arguments = array();
+            if (!is_null($constructorReflection)) {
+                $reflectionParameters = $constructorReflection->getParameters();
+                foreach ($reflectionParameters as $reflectionParameter) {
+                    $paramName = $reflectionParameter->getName();
+                    $paramClass = $reflectionParameter->getClass()->name;
+
+                    $paramInstance = DI::get($paramClass);
+                    $arguments[$paramName] = $paramInstance;
+                }
+            }
+        }
+        $instance = $reflectionClass->newInstanceArgs($arguments);
+        self::$container[$className] = $instance;
+
+        return self::$container[$className];
+    }
+
+    private static function getFromCurrentInstances($className){
+        jlog('CONTAINER', self::$container);
+        //Class parents
+        $implements = class_implements($className);
+        $parents = class_parents($className);
+
+        jlog('IMPLEMENTS', $implements);
+        jlog('PARENTS', $parents);
+
+        $classesToCheck = array_merge(array($className), $implements, $parents);
+        $className = null;
+        foreach ($classesToCheck as $classToCheck) {
+            if (in_array($classToCheck, self::$container)) {
+                return self::$container[$className];
+            }
+        }
+        return false;
     }
 }
