66f01c346ddb2cbb5e6d8692609757b28e12e5b1
Jepi Humet <jepihumet@gmail.com>
2015-12-05 04:23:06 +0100

    [TASK] - Adding extra properties to Controller base

--- JepiFw/System/Controller/Controller.php
+++ JepiFw/System/Controller/Controller.php
@@ -2,8 +2,11 @@
 
 namespace Jepi\Fw\Controller;
 
+use DI\Container;
+use Jepi\Fw\Config\ConfigInterface;
 use Jepi\Fw\Controller\Models;
 use Jepi\Fw\Controller\Views;
+use Jepi\Fw\Router\RouterInterface;
 
 /**
  * Controller.php
@@ -14,12 +17,24 @@ use Jepi\Fw\Controller\Views;
  */
 class Controller implements ControllerInterface {
 
-    protected $routing;
-    protected $session;
+    /**
+     * @var RouterInterface
+     */
+    protected $router;
+    /**
+     * @var ConfigInterface
+     */
+    protected $config;
+
+    /**
+     * @var Container
+     */
+    private $container;
 
 
-    public function __construct() {
-        
+    public function __construct(RouterInterface $router, ConfigInterface $config, Container $container) {
+        $this->router = $router;
+        $this->config = $config;
     }
 
 }
