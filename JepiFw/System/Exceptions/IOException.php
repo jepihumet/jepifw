722391b5a2cb2a50c6a23fe9dfaa801ae1916d21
Jepi Humet <jepihumet@gmail.com>
2015-12-30 13:43:27 +0100

    IOException added

---
+++ JepiFw/System/Exceptions/IOException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * StorageException.php
+ *
+ * @package     JepiFW
+ * @author      Jepi Humet Alsius <jepihumet@gmail.com>
+ * @link        http://jepihumet.com
+ */
+
+namespace Jepi\Fw\Exceptions;
+
+
+class IOException extends JepiException
+{
+    protected $exceptionType = "IO Exception";
+}
