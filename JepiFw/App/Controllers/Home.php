36a4281cc4b612fe41c20950373476b6a369f678
Jepi Humet <jepihumet@gmail.com>
2015-12-31 20:43:15 +0100

    [TASK] - Restablish Demo and Home controllers to pass tests

---
+++ JepiFw/App/Controllers/Home.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+use Jepi\Fw\Model\ModelExample;
+
+/**
+ * Home.php
+ *
+ * @package     JepiFW
+ * @author      Jepi Humet Alsius <jepihumet@gmail.com>
+ * @link        http://jepihumet.com
+ */
+class Home extends \Jepi\Fw\Controller\Controller
+{
+    /**
+     * @Inject
+     * @var ModelExample
+     */
+    private $model;
+
+    /**
+     * @return string
+     */
+    public function index(){
+        return "Hello World";
+    }
+
+    public function welcome(){
+        $this->view->addVar('title', 'JepiFW Template');
+
+        return $this->view->get('bootstrap-template.php', array('content' => 'Hello, world!'));
+    }
+}
