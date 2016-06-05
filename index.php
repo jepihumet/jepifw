1ee9ff0726f8197d43e0c1074c1f0a74aca8f854
Jepi Humet <jepihumet@gmail.com>
2016-06-05 16:41:46 +0200

    Adding coments to index.php

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ function jlog($key, $value){
     echo "$key => ".json_encode($value)."<br>";
 }
 
+/** @var DI\Container $container */
 $container = require_once __DIR__.'/bootstrap.php';
+/** @var Jepi\Fw\FrontController\FrontController $frontController */
 $frontController = $container->get('Jepi\Fw\FrontController\FrontController');
 $frontController->run();
